8fe11df0dd675dc386e395369a50b11b76ca32c2
ashpunia <117685923+ashpunia@users.noreply.github.com>
2024-06-18 14:52:59 -0400

    Add files via upload

--- imagetodecription.py
+++ imagetodecription.py
@@ -1,24 +1,45 @@
-#decription based on the image presented
-
-def generate_description(image):
-    if image == 'CashCheck.png':
-        return "This vending machine accepts cash"
-    if image == 'PhoneCheck.png':
-        return "This vending machine accepts tap to pay"
-    if image == "CreditCheck.png":
-        return "This vending machine accepts cards"
-    if image == "CashX.ong":
-        "This vending machine does not accept cash"
-    if image == "PhoneX.png":
-        "This vending machine does not accept tap to pay"
-    if image == "CreditX.png":
-        "This vending machine does not accept cards"  
-    else:
-        return "Sorry, I don't have a decription for that image."
+def generate_switch_sentence(image1, image2, image3):
+    results = ["This vending machine accepts: "]
     
- 
+    if image1 == "CreditCheck.png":
+        results.append("card")
+    elif image1 == "CreditX.png":
+        results.append("")
+        
+    if image2 == "CashCheck.png":
+        if image1 == "CreditX.png":
+            results.append("cash")   
+        else: 
+            results.append(", cash")
+    elif image2 == "Cashx.png":
+        results.append("")
+        
+    if image3 == "PhoneCheck.png":
+        if image1 == "CreditX.png" and image2 == "CashX.png":
+            results.append("wireless payments")
+        else: 
+            results.append(", wireless payments")
+    elif image3 == "PhoneX.png":
+        results.append("")
+        
+    return "".join(results)
+
+def main():
+    
+    '''
+    First image expects Credit image.
+    Second image expects Cash image.
+    Third image expects phone image.
+    
+    '''
+    # Prompt the user to enter the state of each switch
+    switch1_input = input("First image name: ").strip()
+    switch2_input = input("second image name: ").strip()
+    switch3_input = input("third image name: ").strip()
+
+    # Generate and print the output sentence
+    output_sentence = generate_switch_sentence(switch1_input, switch2_input, switch3_input)
+    print(output_sentence)
 
 if __name__ == "__main__":
-    char = input("Enter the image name: ")
-    decription = generate_description(char)
-    print(decription)
+    main()
