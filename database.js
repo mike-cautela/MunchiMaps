fa682c173fd1ff35099f061facb239865436b3fd
milazz27 <130493712+milazz27@users.noreply.github.com>
2024-07-19 14:49:10 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const dbFile = "./.data/choices.db";
+const dbFile = "/.MunchiData";
 const exists = fs.existsSync(dbFile);
 const sqlite3 = require('sqlite3').verbose();
 const dbWrapper = require("sqlite");
