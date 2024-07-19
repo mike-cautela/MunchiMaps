ed5968f8d3e0446d677f664e5c13e32c0c8ae085
milazz27 <130493712+milazz27@users.noreply.github.com>
2024-07-19 14:38:57 -0400

    Update database.js
    reformatted

--- database.js
+++ database.js
@@ -1,14 +1,24 @@
+const fs = require("fs");
+
+const dbFile = "./.data/choices.db";
+const exists = fs.existsSync(dbFile);
 const sqlite3 = require('sqlite3').verbose();
+const dbWrapper = require("sqlite");
+let db;
 
-// Connect to SQLite database. This will create the database file if it doesn't exist.
-const db = new sqlite3.Database('./database.sqlite', (err) => {
-    if (err) {
-        console.error('Error opening database', err.message);
-    } else {
-        console.log('Connected to the SQLite database.');
-
-        // Create buildings table
-        db.run('CREATE TABLE IF NOT EXISTS building \
+//setting up the default database
+dbWrapper.open({
+  
+  filename: dbFile,
+  driver: sqlite3.Database
+  
+}).then(async dBase => {
+  db = dBase;
+  
+  try{
+    if(!exists) {
+      await db.run (
+        'CREATE TABLE IF NOT EXISTS building \
             (id INTEGER PRIMARY KEY AUTOINCREMENT, \
             name TEXT, \
             x_coord DOUBLE, \
@@ -20,88 +30,66 @@ const db = new sqlite3.Database('./database.sqlite', (err) => {
             num_ratings INTEGER, \
             average_ratings DOUBLE, \
             needs_service CHAR \
-        )', (err) => {
-            if (err) {
-                console.error('Error creating the building table', err.message);
-            } else {
-                console.log('The buildings table was created successfully');
-            }
-        });
-
-        // Create reviews table
-        db.run('CREATE TABLE IF NOT EXISTS review ( \
+        )'
+      )
+      await db.run (
+        'CREATE TABLE IF NOT EXISTS review ( \
             id INTEGER PRIMARY KEY AUTOINCREMENT, \
             building_id INTEGER, \
             product_rating INTEGER, \
             functionality_rating INTEGER, \
             needs_service CHAR, \
             FOREIGN KEY (building_id) REFERENCES buildings(id) \
-        )', (err) => {
-            if (err) {
-                console.error('Error creating review table.', err.message);
-            } else {
-                console.log('The reviews table was created successfully');
-            }
-        });
+        )'
+      )
+    }
+    else {
+      console.log(await db.all('SELECT * from building'))
+    }
+  }
+  catch (dbError){
+      console.catch(dbError);
     }
 });
 
-// Insertion functions
-async function insertBuilding(name, x_coord, y_coord, time_opens, time_closes, num_snack_machines, num_drink_machines, num_ratings, average_ratings, needs_service) {
-    return new Promise((resolve, reject) => {
-        db.run('INSERT INTO building (name, x_coord, y_coord, time_opens, time_closes, num_snack_machines, num_drink_machines, num_ratings, average_ratings, needs_service) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', 
-        [name, x_coord, y_coord, time_opens, time_closes, num_snack_machines, num_drink_machines, num_ratings, average_ratings, needs_service], function(err) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ id: this.lastID, name, x_coord, y_coord, time_opens, time_closes, num_snack_machines, num_drink_machines, num_ratings, average_ratings, needs_service });
-            }
-        });
-    });
-}
-
-async function insertReview(building_id, product_rating, functionality_rating, needs_service) {
-    return new Promise((resolve, reject) => {
-        db.run('INSERT INTO review (building_id, product_rating, functionality_rating, needs_service) VALUES (?, ?, ?, ?)', 
-        [building_id, product_rating, functionality_rating, needs_service], function(err) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ id: this.lastID, building_id, product_rating, functionality_rating, needs_service });
-            }
-        });
-    });
-}
-
-// Functions to fetch all objects of each table
-async function fetchBuildings() {
-    return new Promise((resolve, reject) => {
-        db.all('SELECT * FROM building', [], (err, rows) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(rows);
-            }
-        });
-    });
-}
-
-async function fetchReviews() {
-    return new Promise((resolve, reject) => {
-        db.all('SELECT * FROM review', [], (err, rows) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(rows);
-            }
-        });
-    });
-}
-
-// Defining exports
+//methods to perform on initialized DB
 module.exports = {
-    insertBuilding,
-    insertReview,
-    fetchBuildings,
-    fetchReviews
+  insertBuilding: async (name, x_coord, y_coord, time_opens, time_closes, num_snack_machines, num_drink_machines, num_ratings, average_ratings, needs_service) => {
+    try{
+      await db.run('INSERT INTO building (name, x_coord, y_coord, time_opens, time_closes, num_snack_machines, num_drink_machines, num_ratings, average_ratings, needs_service) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', 
+        [name, x_coord, y_coord, time_opens, time_closes, num_snack_machines, num_drink_machines, num_ratings, average_ratings, needs_service]); 
+    }
+    catch(dbError) {
+      console.catch(dbError);
+    }
+  },
+  
+  insertReview: async (building_id, product_rating, functionality_rating, needs_service) => {
+    try {
+      await db.run('INSERT INTO review (building_id, product_rating, functionality_rating, needs_service) VALUES (?, ?, ?, ?)', 
+        [building_id, product_rating, functionality_rating, needs_service]);
+    }
+    catch (dbError) {
+      console.catch(dbError);
+    }
+  },
+
+  fetchAllBuildings: async() => {
+    try{
+      return await db.all('SELECT * FROM building');
+    }
+    catch(dbError){
+      console.log(dbError);
+    }
+  },
+  
+  fetchAllReviews: async() => {
+    try{
+      return await db.all('SELECT * FROM review');
+    }
+    catch(dbError){
+      console.log(dbError);
+    }
+  },
+
 };
