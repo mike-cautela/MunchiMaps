49f52d0e050d20afcb5f01f5c835a09bc91e4fbb
milazz27 <130493712+milazz27@users.noreply.github.com>
2024-08-08 20:46:46 -0400

    Update server.js
    updated to use fastify

--- scripts/server.js
+++ scripts/server.js
@@ -2,22 +2,43 @@ console.log("Server.js running");
 
 // We are at the mercy of the package.json file. DO NOT anger it.
 
-const express = require('express');
-const path = require('path');
-const app = express();
+const fastify = require("fastify")({
+  logger:true
+});
 
+const path = require('path');
+const dbFunctions = require("../scripts/database.js");
 const db = require("../scripts/database.js");
 
 console.log("Boogly boo");
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '../Website/new.html'));
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, '../Website'),
+  prefix: '/',
 });
+
+// Routes
+const indexRouter = require('./routes/index');
+fastify.register(indexRouter);
+
+fastify.register(require("@fastify/formbody"));
+
+// OnRoute hook to list endpoints
+const routes = { endpoints: [] };
+fastify.addHook("onRoute", routeOptions => {
+  routes.endpoints.push(routeOptions.method + " " + routeOptions.path);
+});
+
+// Register route modules
+fastify.register(require('../scripts/buildings.js'));
+fastify.register(require('../scripts/reviews.js'));
 
 db.initializeDatabase();
 
-app.use(express.static("./"));
-
-app.listen(5000, () => {
-  console.log('Server started at http://localhost:5000/');
+fastify.listen({ port: 5000 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server started at ${address}`);
 });
