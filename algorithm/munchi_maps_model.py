1559cc1479a0126e00ca006573cc1dabcb844dc0
RyanLil-Xvx <lilryan988@gmail.com>
2024-10-09 13:40:20 -0400

    Update the MunchiMaps_model and test cases

--- algorithm/munchi_maps_model.py
+++ algorithm/munchi_maps_model.py
@@ -3,15 +3,16 @@ import csv
 class MunchiMaps_model(object):
     def __init__(self, csv_file):
         self.data = csv_file
-        self.building = []
-        self.amount = []
-        self.drink = []
-        self.food = []
-        self.location_description = []
-        self.hours_of_operation = []
-        self.access_information = []
-        self.coordinates = []
+        self.building = list()
+        self.amount = list()
+        self.drink = list()
+        self.food = list()
+        self.location_description = list()
+        self.hours_of_operation = list()
+        self.access_information = list()
+        self.coordinates = list()
         self.load_data() # load the data from the CSV file
+        self.vendings_collection = list() # a list of dictionaries [{}], store all the vending machines info
     
     # read the CSV file and populate the lists
     def load_data(self):
@@ -30,3 +31,33 @@ class MunchiMaps_model(object):
     # print out all the data we read from the CSV file
     def __str__(self):
         return "{}:\n\nBuilding: {}\n\nAmount: {}\n\nDrink: {}\n\nFood: {}\n\nLocation description: {}\n\nHours of operation: {}\n\nAccess information: {}\n\nCoordinates: {}".format(self.data, self.building, self.amount, self.drink, self.food, self.location_description, self.hours_of_operation, self.access_information, self.coordinates)
+    
+    # collect all the vending machines info into self.vendings, a list of dictionaries
+    def collect_vendings(self):
+        vendings = []
+        if (len(self.building) == len(self.amount) == len(self.drink) == len(self.food) == len(self.location_description) == len(self.hours_of_operation) == len(self.access_information) == len(self.coordinates)):
+            for i in range(len(self.building)):
+                vending = {
+                    "Building": self.building[i],
+                    "Amount": self.amount[i],
+                    "Drink": self.drink[i],
+                    "Food": self.food[i],
+                    "Location description": self.location_description[i],
+                    "Hours of operation": self.hours_of_operation[i],
+                    "Access information": self.access_information[i],
+                    "Coordinates": self.coordinates[i]
+                }
+                vendings.append(vending)
+            self.vendings_collection = vendings
+        else:
+            print("Error: The length of the lists are not equal. Please check the data.\n")
+    
+    # print the certain vending machine info
+    def get_vending_info(self, vending_machine_location):
+        for i in self.vendings_collection:
+            if (i["Building"] == vending_machine_location):
+                return "Building: {}\nAmount: {}\nDrink: {}\nFood: {}\nLocation description: {}\nHours of operation: {}\nAccess information: {}\n".format(i["Building"], i["Amount"], i["Drink"], i["Food"], i["Location description"], i["Hours of operation"], i["Access information"])
+        return "The vending machines' location is not found.\n"
+        
+    
+    
--- algorithm/test_MunchiMaps_model.py
+++ algorithm/test_MunchiMaps_model.py
@@ -1,5 +1,21 @@
 from munchi_maps_model import MunchiMaps_model
 if __name__ == "__main__":
-    data = input("Type the name of the file: ").strip() # strip()函数用于去除字符串两端的空格
+    data = input("Type the name of the file: ").strip() # strip() removes leading and trailing whitespaces
     vending_machine_data = MunchiMaps_model(data)
-    print(vending_machine_data)
+    # print(vending_machine_data)
+    vending_machine_data.collect_vendings()
+    # print(vending_machine_data.vendings_collection)
+    
+    # test: check function get_vending_info()
+    test1 = vending_machine_data.get_vending_info("DCC")
+    if (test1 == "The vending machines' location is not found.\n"):
+        print("get_vending_info() test 1 passed.")
+    else:
+        print("get_vending_info() test 1 failed.")
+    test2 = vending_machine_data.get_vending_info("Rensselaer Student Union")
+    if (test2 == "Building: Rensselaer Student Union\nAmount: 2\nDrink: TRUE\nFood: TRUE\nLocation description: 1st floor\nHours of operation: Monday-Friday 7AM-12AM|Weekend 9AM-12AM\nAccess information: RPI ID\n"):
+        print("get_vending_info() test 2 passed.")
+    else:
+        print("get_vending_info() test 2 failed.")
+        
+    # test:
