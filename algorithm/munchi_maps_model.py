1f2cd9b6cd86f8b76539f33b1216cc75daa0d88f
RyanLil-Xvx <lilryan988@gmail.com>
2024-10-10 01:26:29 -0400

    Update the fully filtration system and update the test cases.

--- algorithm/munchi_maps_model.py
+++ algorithm/munchi_maps_model.py
@@ -1,4 +1,5 @@
-import csv
+import csv # read the CSV file
+from datetime import datetime # get the current date and time
 
 class MunchiMaps_model(object):
     def __init__(self, csv_file):
@@ -52,12 +53,92 @@ class MunchiMaps_model(object):
         else:
             print("Error: The length of the lists are not equal. Please check the data.\n")
     
-    # print the certain vending machine info
+    # helper function: print the certain vending machine info
     def get_vending_info(self, vending_machine_location):
         for i in self.vendings_collection:
             if (i["Building"] == vending_machine_location):
                 return "Building: {}\nAmount: {}\nDrink: {}\nFood: {}\nLocation description: {}\nHours of operation: {}\nAccess information: {}\n".format(i["Building"], i["Amount"], i["Drink"], i["Food"], i["Location description"], i["Hours of operation"], i["Access information"])
         return "The vending machines' location is not found.\n"
+    
+    # helper function: check if the current time is within the vending machine's hours of operation
+    def check_time(self, time, day, time_range):
+        # process the time_range string
+        day_range, hours_range = time_range.split() # split the time_range by space
+        start_time, end_time = hours_range.split("–") # split the hours_range by "–"
         
+        # make sure the given day and the day_range are the same, so ignore the upper and lower case
+        if (day.lower() != day_range.lower()):
+            return False
+        
+        # need to convert the time to 24-hour format
+        if ("AM" in start_time):
+            start_hour = int(start_time.replace("AM", ""))
+            # corner case: 12AM equals to 00
+            if (start_hour == 12):
+                start_hour = "00"
+            else:
+                # f"{start_hour:02d}" means insert the value of the variable start_hour into the string and format it.
+                # 02 means fill the number to two digits, and fill it with leading zeros if it is less than two digits.
+                start_hour = f"{start_hour:02d}"
+        # PM
+        else:
+            start_hour = int(start_time.replace("PM", ""))
+            # corner case: 12PM equals to 12(noon)
+            if (start_hour == 12):
+                start_hour = "12"
+            else:
+                start_hour += 12 # add 12 hours to other PM times
+                start_hour = f"{start_hour:02d}"
+        if ("AM" in end_time):
+            end_hour = int(end_time.replace("AM", ""))
+            # corner case: 12AM equals to 00
+            if (end_hour == 12):
+                end_hour = "00"
+            else:
+                end_hour = f"{end_hour:02d}"
+        # PM
+        else:
+            end_hour = int(end_time.replace("PM", ""))
+            # corner case: 12PM equals to 12(noon)
+            if (end_hour == 12):
+                end_hour = "12"
+            else:
+                end_hour += 12
+                end_hour = f"{end_hour:02d}"
+        # handling multi-day situations, for example: 7AM–12AM (early morning of the next day)
+        if (end_hour == "00"):
+            end_hour = "24"
+        # check if the current time is within the vending machine's hours of operation
+        # given time is available
+        if (start_hour <= time < end_hour):
+            return True
+        # given time is earlier than the start time
+        elif (time < start_hour):
+            return False
+        # given time is later than the end time
+        elif (time >= end_hour):
+            return False
     
+    # helper function: A real time system to check if the current time is within the vending machine's hours of operation  
+    def time_system(self, time, day_of_week, vending_machine_location):
+        for i in self.vendings_collection:
+            if (i["Building"] == vending_machine_location):
+                time_str = i["Hours of operation"]
+                time_list = time_str.split("|") # hours is a list of strings
+        for current_day in time_list:
+            if (day_of_week in current_day):
+                if (self.check_time(time, day_of_week, current_day) == True):
+                    return True
+        return False
     
+    # filter out all the vending machines that are available and print out their info
+    def filtration_system(self):
+        time = datetime.now().strftime("%H") # get the current time, only want the hour
+        day_of_week = datetime.now().strftime("%A") # get the current day of the week
+        available_vendings = [] # store all the location of the vending machines that are available
+        for location in self.building:
+            if (self.time_system(time, day_of_week, location) == True):
+                available_vendings.append(location)
+        # print out all the available vending machines' info  
+        for location in available_vendings:
+            self.get_vending_info(location)
