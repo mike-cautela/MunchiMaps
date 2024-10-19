0ce597392df58e01c593e64e8dfe7f922d1892f1
Chubbyguin <chubbyguin@gmail.com>
2024-10-18 20:33:53 -0400

    Finished Creating all the buildings objects

--- Pathfinder/pathfinder.cpp
+++ Pathfinder/pathfinder.cpp
@@ -7,7 +7,27 @@
 #include "json.hpp"
 
 
-
+std::vector<Locations> getnodes(std::ifstream &infile){
+    nlohmann::json jsonData;
+    infile >> jsonData;
+    std::vector<Locations> locations;
+    int num = 0;
+    for(const auto& route : jsonData["routes"]){
+        if(route["hasVendingMachine"] == "true"){
+            Locations loc;
+            loc.setName(route["name"]);
+            std::cout << route["name"] << std::endl;
+            loc.setLocationX(stod(route["location"][0].get<std::string>()));
+            loc.setLocationY(stod(route["location"][1].get<std::string>()));
+            loc.food(route["food"] == "true");
+            loc.drinks(route["drink"] == "true");
+            locations.push_back(loc);
+        }
+        num++;
+    }
+    std::cout << locations.size();
+    return locations;
+}
 
 int main(){
     std::ifstream infile("nodes.json");
