92b0f7a60b9a81dca50fafcfc42af260dc575bd0
phanl2 <phanl2@rpi.edu>
2024-11-19 16:41:15 -0500

    Update App.jsx

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -6,6 +6,8 @@ import './styles/loading_animation_stylesheet.css';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [showSearchPopup, setShowSearchPopup] = useState(false);
+  const [showReportPopup, setShowReportPopup] = useState(false);
 
   const buildings = [
     { name: 'Folsom Library', drink: true, food: true },
@@ -160,39 +162,3 @@ function App() {
 }
 
 export default App;
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
