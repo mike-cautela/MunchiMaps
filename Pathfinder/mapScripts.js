8ead2bf75f7fd2059ebaca270844a5065d011e60
Chubbyguin <chubbyguin@gmail.com>
2024-12-01 00:36:33 -0500

    dynamically added all the markers from my json file to the map so they appear as points

--- Pathfinder/mapScripts.js
+++ Pathfinder/mapScripts.js
@@ -4,14 +4,21 @@ $(document).ready(function(){
         url: "./nodes.json",
         dataType: "json",
         success: function(responseData, status){
-            var output = "<ul>";
+            var output = "";
+            var counter = 0;
             $.each(responseData.machines, function(i, machine){
                 var lat = parseFloat(machine.location[0]);
                 var lon = parseFloat(machine.location[1]);
-                output += 'var marker = L.marker(['+ lat + ',' + lon +']).addTo(map);'
+                counter++;
+                output += 'var marker' + counter + ' = L.marker(['+ lat + ',' + lon +']).addTo(map);';
                 
             });
-            $('#current').html(output);
+            var scriptTag = document.createElement('script');
+            scriptTag.textContent = output;
+            document.getElementById('markers').appendChild(scriptTag); // Append the new script to the markers script tag
+            
+            console.log(output);
+
         }
     })
 })
